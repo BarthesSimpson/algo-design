9585e1fd1cb40a9fadfdafeb26c569a2ede1bf03
BarthesSimpson <d.s.frenk@gmail.com>
2019-08-18 15:27:27 -0700

    Add spec for multiset permutations

--- multisets/multisets.go
+++ multisets/multisets.go
@@ -1,1 +1,21 @@
 package multisets
+
+import (
+	"errors"
+	"fmt"
+)
+
+/*
+Use backtracking to generate all permutations of the given multiset.
+A multiset is a set that can multiple instances of any member.
+*/
+
+// Permutations returns a slice of slices, each representing a unique permutation
+// of the multiset ms
+func Permutations(ms [][]int) ([][]int, error) {
+	if len(ms) < 2 {
+		return ms, nil
+	}
+	return nil, errors.New("Not yet implemented")
+}
+
--- multisets/multisets_test.go
+++ multisets/multisets_test.go
@@ -1,1 +1,68 @@
 package multisets
+
+import (
+	"testing"
+
+	. "github.com/franela/goblin"
+)
+
+func TestPermutations(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Permutations tests", func() {
+		g.It("works for the empty set", func() {
+			res, err := Permutations([][]int{})
+			exp := [][]int{}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+		g.It("works for a set with one member", func() {
+			res, err := Permutations([][]int{[]int{1}})
+			exp := [][]int{[]int{1}}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+		g.Xit("returns a single result if all members in ms are identical", func() {
+			res, err := Permutations([][]int{[]int{1, 1, 1}})
+			exp := [][]int{[]int{1, 1, 1}}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+		g.Xit("works for {1, 2, 3}", func() {
+			res, err := Permutations([][]int{[]int{1, 2, 3}})
+			exp := [][]int{
+				[]int{1, 2, 3}, []int{1, 3, 2},
+				[]int{2, 1, 3}, []int{2, 3, 1},
+				[]int{3, 1, 2}, []int{3, 2, 1},
+			}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+		g.Xit("works for {1, 1, 2}", func() {
+			res, err := Permutations([][]int{[]int{1, 1, 2}})
+			exp := [][]int{
+				[]int{1, 1, 2}, []int{1, 2, 1}, []int{2, 1, 1},
+			}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+		g.Xit("works for {1, 1, 1, 2}", func() {
+			res, err := Permutations([][]int{[]int{1, 1, 1, 2}})
+			exp := [][]int{
+				[]int{1, 1, 1, 2}, []int{1, 1, 2, 1},
+				[]int{1, 2, 1, 1}, []int{2, 1, 1, 1},
+			}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+		g.Xit("works for {1, 1, 2, 2}", func() {
+			res, err := Permutations([][]int{[]int{1, 1, 2, 2}})
+			exp := [][]int{
+				[]int{1, 1, 2, 2}, []int{1, 2, 1, 2},
+				[]int{1, 2, 2, 1}, []int{2, 1, 1, 2},
+				[]int{2, 1, 2, 1}, []int{2, 2, 1, 1},
+			}
+			g.Assert(err == nil).IsTrue()
+			g.Assert(exp).Equal(res)
+		})
+	})
+}
